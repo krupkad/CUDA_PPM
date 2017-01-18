d61d26e8506acc0ed89e0d4a9d0f39588a2e6740
nikolai <nikolai@SEASNet-62-15.seas.upenn.edu>
2017-01-18 10:14:18 -0500

    clean up, fix argc bug

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,39 @@
 #include "gui.hpp"
 
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
+  printf("argc = %d\n", argc);
+  if (argc > 2) {
     
     PPM *ppm = new PPM(false);
+    const char *fName = argv[1];
+    const char *opt = argv[2];
     
     int nBasis, nSamp, nSub;
-    int r2 = atoi(argv[3]), r3 = atoi(argv[4]);
-    if (!strcmp(argv[2], "--samp")) {
+    if (!strcmp(opt, "--samp")) {
       if (argc != 7) {
-        printf("usage: %s --samp [start end] deg sub\n", argv[0]);
+        printf("usage: %s --samp start end deg sub\n", argv[0]);
         delete ppm;
         return 0;
       }
       
+      int r2 = atoi(argv[3]), r3 = atoi(argv[4]);
       nBasis = atoi(argv[5]);
       nSub = atoi(argv[6]);
       for (int i = r2; i <= r3; i++) {
         nSamp = nSub * (1 << i);
-        ppm->rebuild(argv[1], nBasis, nSamp, nSub);
+        ppm->rebuild(fName, nBasis, nSamp, nSub);
 
         printf("%d -> ", nSamp);
         try {
           ppm->useTessSM = false;
-          float dt = ppm->update(-1, 0.0f,.1f);
+          float dt = ppm->update(-1, 0.0f,.01f);
           printf("%f ", dt);
         } catch (const std::exception &e) {
           printf("%s ", e.what());
         }
         try {
           ppm->useTessSM = true;
-          float dt = ppm->update(-1, 0.0f,.1f);
+          float dt = ppm->update(-1, 0.0f,.01f);
           printf("%f\n", dt);
         } catch (const std::exception &e) {
           printf("%s\n", e.what());
@@ -41,29 +44,30 @@ int main(int argc, char *argv[]) {
       }
     }
     
-    else if (!strcmp(argv[2], "--deg")) {
+    else if (!strcmp(opt, "--deg")) {
       if (argc != 6) {
-        printf("usage: %s --deg [start end] sub\n", argv[0]);
+        printf("usage: %s --deg start end sub\n", argv[0]);
         delete ppm;
         return 0;
       }
       
+      int r2 = atoi(argv[3]), r3 = atoi(argv[4]);
       nSub = atoi(argv[5]);
       for (int i = r2; i <= r3; i++) {
         nSamp = nBasis = 2*(2 + i);
-        ppm->rebuild(argv[1], nBasis, nSamp, nSub);
+        ppm->rebuild(fName, nBasis, nSamp, nSub);
         
         printf("%d -> ", nBasis);
         try {
           ppm->useTessSM = false;
-          float dt = ppm->update(-1, 0.0f,.1f);
+          float dt = ppm->update(-1, 0.0f,.01f);
           printf("%f ", dt);
         } catch (const std::exception &e) {
           printf("%s ", e.what());
         }
         try {
           ppm->useTessSM = true;
-          float dt = ppm->update(-1, 0.0f,.1f);
+          float dt = ppm->update(-1, 0.0f,.01f);
           printf("%f\n", dt);
         } catch (const std::exception &e) {
           printf("%s\n", e.what());
@@ -72,30 +76,31 @@ int main(int argc, char *argv[]) {
       }
     }
     
-    else if (!strcmp(argv[2], "--sub")) {
+    else if (!strcmp(opt, "--sub")) {
       if (argc != 7) {
-        printf("usage: %s --sub [start end] deg samp\n", argv[0]);
+        printf("usage: %s --sub start end deg samp\n", argv[0]);
         delete ppm;
         return 0;
       }
       
+      int r2 = atoi(argv[3]), r3 = atoi(argv[4]);
       nBasis = atoi(argv[5]);
       nSamp = atoi(argv[6]);
       for (int i = r2; i <= r3; i++) {
         nSub = (1 << i);
-        ppm->rebuild(argv[1], nBasis, nSamp, nSub);
+        ppm->rebuild(fName, nBasis, nSamp, nSub);
         
         printf("%d -> ", nSub);
         try {
           ppm->useTessSM = false;
-          float dt = ppm->update(-1, 0.0f,.1f);
+          float dt = ppm->update(-1, 0.0f,.01f);
           printf("%f ", dt);
         } catch (const std::exception &e) {
           printf("%s ", e.what());
         }
         try {
           ppm->useTessSM = true;
-          float dt = ppm->update(-1, 0.0f,.1f);
+          float dt = ppm->update(-1, 0.0f,.01f);
           printf("%f\n", dt);
         } catch (const std::exception &e) {
           printf("%s\n", e.what());
@@ -103,7 +108,7 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    else if (!strcmp(argv[2], "--ix")) {
+    else if (!strcmp(opt, "--ix")) {
       if (argc != 5) {
         printf("usage: %s --ix sub count\n", argv[0]);
         delete ppm;
@@ -113,7 +118,7 @@ int main(int argc, char *argv[]) {
       nBasis = 4;
       nSamp = 4;
       nSub = (1 << atoi(argv[3]));
-      ppm->rebuild(argv[1], nBasis, nSamp, nSub);
+      ppm->rebuild(fName, nBasis, nSamp, nSub);
       ppm->update(-1, 0.0f,.1f);
 
       float dt = 0, t;
@@ -131,7 +136,7 @@ int main(int argc, char *argv[]) {
     }
 
     else {
-      printf("usage: %s [--samp, --deg, --sub]\n", argv[0]);
+      printf("usage: %s [--samp, --deg, --sub, --ix]\n", argv[0]);
     }
     
     delete ppm;
@@ -146,37 +151,3 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-/*
-void resizeCallback(GLFWwindow* window, int x, int y) {
-  xSize = x;
-  ySize = y;
-  glViewport(0,0,x,y);
-  updateCamera();
-}
-
-void errorCallback(int error, const char *description) {
-  fprintf(stderr, "GLFW error %d: %s\n", error, description);
-}
-
-void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
-  leftMousePressed = (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS);
-  rightMousePressed = (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS);
-}
-
-void mousePositionCallback(GLFWwindow* window, double xpos, double ypos) {
-  if (leftMousePressed) {
-    phi += (xpos - lastX) / xSize;
-    theta -= (ypos - lastY) / ySize;
-    updateCamera();
-  }
-
-  lastX = xpos;
-  lastY = ypos;
-}
-
-void scrollCallback(GLFWwindow* window, double dx, double dy) {
-  zoom += 30.0f*dy / ySize;
-  zoom = std::fmax(0.01f, std::fmin(zoom, 100.0f));
-  updateCamera();
-}
-*/
